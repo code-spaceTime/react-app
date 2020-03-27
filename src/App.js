7eba336901af85b661084b6dc4a01289bf62f215
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-03-26 21:59:35 -0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, {Component} from 'react';
 import './App.css';
 import Todos from './components/Todos';
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
+import { v4 as uuidv4 } from 'uuid'
 
 class App extends Component {
   state = {
     todos: [
       { 
-        id: 1,
+        id: uuidv4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id:2,
+        id: uuidv4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id:3,
+        id: uuidv4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -30,19 +33,30 @@ class App extends Component {
       }
       return todo;
     })})
-    console.log(id)
   }
 
   //Delete todo
   delTodo = (id) => {
-    console.log(id)
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
   
+  addTodo = (title) =>{
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
+  }
 
   render() {
   return (
     <div className="App">
+      <div className="container"> 
+       <Header />
+       <AddTodo addTodo={this.addTodo}/>
        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
     </div>
   );
 }
